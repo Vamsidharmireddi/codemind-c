a0e535fd5cdec5e6fffc8e73913cd2d0b8165c82
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-03-08 19:18:30 +0530

    #include<stdio.h> int main() {     int n,i,a,b,sum;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a,&b);         sum=a+b;         printf("%d ",sum);         sum=0;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,sum;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&a,&b);
+        sum=a+b;
+        printf("%d
+",sum);
+        sum=0;
+    }
+}
