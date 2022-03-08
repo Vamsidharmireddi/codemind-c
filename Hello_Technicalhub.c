a011604f156739de835803142494b47775038427
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-03-08 21:36:32 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100],str1[100]="Hello Technicalhub";     scanf("%[^ ]s",str);     printf("%s ",str1);     printf("%s ",str); }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],str1[100]="Hello Technicalhub";
+    scanf("%[^
+]s",str);
+    printf("%s
+",str1);
+    printf("%s
+",str);
+}
