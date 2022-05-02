954e8e6bde39bc61f656796f07d49d4adf103ed7
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-05-02 10:42:08 +0530

    #include<stdio.h> int main() {     int r,c,i,sum=0,a[100][100],j;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<c;j++)     {         for(i=0;i<r;i++)         {            sum=sum+a[i][j];         }         printf("%d ",sum);         sum=0;     } }

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,sum=0,a[100][100],j;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(j=0;j<c;j++)
+    {
+        for(i=0;i<r;i++)
+        {
+           sum=sum+a[i][j];
+        }
+        printf("%d ",sum);
+        sum=0;
+    }
+}
