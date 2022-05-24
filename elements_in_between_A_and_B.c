90e9b108295a52c68ecaea1c56b864231c0ce66f
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-05-24 15:53:52 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c,b,k=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&c,&b);     for(i=1;i<=n;i++)     {         if(a[i]>=c && a[i]<=b)         {             k=k+1;            printf("%d ",a[i]);         }     }     if(k==0)     {         printf("-1");     } }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],c,b,k=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&c,&b);
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]>=c && a[i]<=b)
+        {
+            k=k+1;
+           printf("%d ",a[i]);
+        }
+    }
+    if(k==0)
+    {
+        printf("-1");
+    }
+}
