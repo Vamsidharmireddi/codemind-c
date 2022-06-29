db9cdd1abaadb95f72caa7ad73bb52b2e7548fa8
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-29 21:48:21 +0530

    #include<stdio.h> int main() {     int r,c,a[100][100],i,j,k,max=0,s,max1;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         k=0;         for(j=0;j<c;j++)         {             k=k+a[i][j];         }         if(max<k)         {             max=k;         }     }     for(j=0;j<c;j++)     {         s=0;         for(i=0;i<r;i++)         {             s=s+a[i][j];         }         if(max1<s)         {             max1=s;         }     }     if(max>max1)     {         printf("%d",max);     }     else     {         printf("%d",max1);     }
    }

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -1,42 +1,46 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-    int r,c,i,j,a[100][100],s=0,max=0,s1=0,max1=0;
+    int r,c,a[100][100],i,j,k,max=0,s,max1;
     scanf("%d%d",&r,&c);
-    for(i=1;i<=r;i++)
+    for(i=0;i<r;i++)
     {
-        for(j=1;j<=c;j++)
+        for(j=0;j<c;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=1;i<=r;i++)
+    for(i=0;i<r;i++)
+    {
+        k=0;
+        for(j=0;j<c;j++)
+        {
+            k=k+a[i][j];
+        }
+        if(max<k)
+        {
+            max=k;
+        }
+    }
+    for(j=0;j<c;j++)
     {
         s=0;
-        for(j=1;j<=c;j++)
+        for(i=0;i<r;i++)
         {
             s=s+a[i][j];
         }
-        if(max<s)
+        if(max1<s)
         {
-            max=s;
-        }
-    }
-     for(j=1;j<=c;j++)
-    {
-        s1=0;
-        for(i=1;i<=r;i++)
-        {
-            s1=s1+a[i][j];
-        }
-        if(max1<s1)
-        {
-            max1=s1;
+            max1=s;
         }
     }
     if(max>max1)
-    printf("%d",max);
+    {
+        printf("%d",max);
+    }
     else
-    printf("%d",max1);
+    {
+        printf("%d",max1);
+    }
+    
 }
