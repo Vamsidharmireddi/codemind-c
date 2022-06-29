444707e7bab674bdbc0243dc99a1b6df11d321ca
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-06-29 21:50:48 +0530

    #include<stdio.h> int main() {     int r,c,a[100][100],i,j,k,max=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<c;j++)     {         k=0;         for(i=0;i<r;i++)         {             k=k+a[i][j];         }         if(max<k)         {             max=k;         }     }     printf("%d",max); }

--- Maximum_sum_of_column_wise_sum_of_a_matrix.c
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-    int r,c,i,j,a[100][100],s=0,max=0;
+    int r,c,a[100][100],i,j,k,max=0;
     scanf("%d%d",&r,&c);
-    for(i=1;i<=r;i++)
+    for(i=0;i<r;i++)
     {
-        for(j=1;j<=c;j++)
+        for(j=0;j<c;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(j=1;j<=c;j++)
+    for(j=0;j<c;j++)
     {
-        s=0;
-        for(i=1;i<=r;i++)
+        k=0;
+        for(i=0;i<r;i++)
         {
-            s=s+a[i][j];
+            k=k+a[i][j];
         }
-        if(max<s)
+        if(max<k)
         {
-            max=s;
+            max=k;
         }
     }
     printf("%d",max);
