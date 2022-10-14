440ac007cee20082be0135b24a56b02ac8c47f4e
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-10-14 21:40:53 +0530

    #include<stdio.h> void select_sort(int *arr,int n) {     for(int i=0;i<n;i++)     {         int min=i;         for(int j=i;j<n;j++)         {             if(arr[j]<arr[min])             {                 min=j;             }         }         int temp=arr[i];         arr[i]=arr[min];         arr[min]=temp;     } } int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)         scanf("%d",&arr[i]);     for(int i=0;i<n;i++)         arr[i]=arr[i]*arr[i];     select_sort(arr,n);     for(int i=0;i<n;i++)         printf("%d ",arr[i]); }

---
+++ _Squares_of_a_Sorted_Array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void select_sort(int *arr,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int min=i;
+        for(int j=i;j<n;j++)
+        {
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++)
+        arr[i]=arr[i]*arr[i];
+    select_sort(arr,n);
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+}
