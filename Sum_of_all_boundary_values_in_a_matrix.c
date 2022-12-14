c1a758fe940a1c050f942019e4c28a1d119a2b1f
Vamsidharmireddi <96384144+Vamsidharmireddi@users.noreply.github.com>
2022-12-14 17:31:46 +0530

    #include<iostream> using namespace std; int main() {     int r,c,a[100][100],i,j,s=0;     scanf("%d%d",&r,&c);     for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             if(i==0 || i==r-1 || j==0 || j==r-1)             {                 s=s+a[i][j];             }         }     }     printf("%d",s); }

--- Sum_of_all_boundary_values_in_a_matrix.c
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -1,29 +1,25 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    int r,c,a[100][100],i,j,s=0,k=0;
+    int r,c,a[100][100],i,j,s=0;
     scanf("%d%d",&r,&c);
-    for(i=0;i<r;i++)
+    for(i=0;i<c;i++)
     {
         for(j=0;j<c;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=0;i<r;i++)
+    for(i=0;i<c;i++)
     {
         for(j=0;j<c;j++)
         {
-            k=k+a[i][j];
+            if(i==0 || i==r-1 || j==0 || j==r-1)
+            {
+                s=s+a[i][j];
+            }
         }
     }
-    for(i=1;i<r-1;i++)
-    {
-        for(j=1;j<c-1;j++)
-        {
-            s=s+a[i][j];
-        }
-    }
-    printf("%d",k-s);
-    
+    printf("%d",s);
 }
